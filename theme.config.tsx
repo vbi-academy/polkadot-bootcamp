9382fc637ebfbb7bb1c62a0c4ad60ed52ebbd939
CocDap <hodinhdung1997@gmail.com>
2024-08-07 16:26:25 +0700

    feat: add section 1 and section 2 base

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Movement with Aptos</span>,
+  logo: <span>Polkadot Bootcamp 2024</span>,
   project: {
     link: 'https://github.com/openedu101/polkadot-bootcamp',
   },
   chat: {
-    link: 'https://discord.gg/RgGUcPdzbc',
+    link: 'https://discord.gg/K4GYwvPZs4',
   },
   docsRepositoryBase: 'https://github.com/openedu101/polkadot-bootcamp/blob/main',
   footer: {
